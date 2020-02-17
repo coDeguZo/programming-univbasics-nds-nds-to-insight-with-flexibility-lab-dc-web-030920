8dd3da7444cb8e38d618c90820d1e62d6137913c
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-17 22:50:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,12 +1,6 @@
 # Provided, don't edit
 require 'directors_database'
 
-def print(src)
-  pp src
-end
-
-print(directors_database)
-
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
 
