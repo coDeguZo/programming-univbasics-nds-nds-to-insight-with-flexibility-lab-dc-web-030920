a9ca9d0704c4b0990a58d0559ab59c483e8d3708
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-17 23:06:41 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,12 +49,12 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   new_array = []
-    index = 0
-    while index < movies_collection.count do
-        new_array << movie_with_director_name(name, movie_data)
-        index += 1
-    end
-    new_array
+  index = 0
+  while index < movies_collection.count do
+    new_array << movie_with_director_name(name, movie_data)
+    index += 1
+  end
+  return new_array
 end
 
 
